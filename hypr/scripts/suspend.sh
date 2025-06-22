380b64f3bc78286436d507462323fb4291d67170
anubhab7111 <anubhabdas111@gmail.com>
2025-06-22 22:41:00 +0530

    added animations and organized files

--- hypr/scripts/suspend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-swayidle -w \
-  timeout 900 'systemctl suspend' \
-  resume 'notify-send "Resumed"'
-
--- hypr/scripts/tmp/suspend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-swayidle -w \
-timeout 300 'hyprlock' \
-timeout 500 ' hyprctl dispatch dpms off' \
-timeout 12000 'systemctl suspend' \
-resume ' hyprctl dispatch dpms on' \
-before-sleep 'hyprlock'
